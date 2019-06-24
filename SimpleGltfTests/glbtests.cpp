1c6a8a3a7ac17f3a0ddbba21face30c80ea0d15f
ousttrue <ousttrue@gmail.com>
2019-06-24 12:13:24 +0900

    clang-format

--- SimpleGltfTests/glbtests.cpp
+++ SimpleGltfTests/glbtests.cpp
@@ -2,43 +2,41 @@
 #include <catch2/catch.hpp>
 
 #include "../SimpleGltf/simplegltf.h"
+#include <fstream>
 #include <string_view>
 #include <vector>
-#include <fstream>
 
-auto read_allbytes(std::string_view path) -> decltype(auto)
-{
-    std::vector<std::byte> data;
 
-    // open the file for binary reading
-    std::ifstream file;
-    file.open(path.data(), std::ios_base::binary);
-    if (file.is_open())
-    {
-        // get the length of the file
-        file.seekg(0, std::ios::end);
-        size_t fileSize = file.tellg();
-        file.seekg(0, std::ios::beg);
+auto read_allbytes(std::string_view path) -> decltype(auto) {
+  std::vector<std::byte> data;
 
-        // read the file
-        data.resize(fileSize);
-        file.read(reinterpret_cast<char *>(&data[0]), fileSize);
+  // open the file for binary reading
+  std::ifstream file;
+  file.open(path.data(), std::ios_base::binary);
+  if (file.is_open()) {
+    // get the length of the file
+    file.seekg(0, std::ios::end);
+    size_t fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
 
-        file.close();
-    }
+    // read the file
+    data.resize(fileSize);
+    file.read(reinterpret_cast<char *>(&data[0]), fileSize);
 
-    return data;
+    file.close();
+  }
+
+  return data;
 }
 
 auto ROOT_DIR = std::string("..\\..\\");
 
-void test_gltf(const std::string &test_path)
-{
-    auto path = ROOT_DIR + test_path;
-    auto bytes = read_allbytes(path);
-    simple_gltf::Storage storage;
-    storage.parse(bytes.data(), bytes.size(), path);
-    REQUIRE(!bytes.empty());
+void test_gltf(const std::string &test_path) {
+  auto path = ROOT_DIR + test_path;
+  auto bytes = read_allbytes(path);
+  simple_gltf::Storage storage;
+  storage.parse(bytes.data(), bytes.size(), path);
+  REQUIRE(!bytes.empty());
 }
 
 // TEST_CASE("SimpleMeshes", "[gltf]")
@@ -53,7 +51,6 @@ void test_gltf(const std::string &test_path)
 //     auto a = 0;
 // }
 
-TEST_CASE("Cube", "[glb]")
-{
-    test_gltf("dependencies\\glTF-Sample-Models\\2.0\\Box\\glTF-Binary\\Box.glb");
+TEST_CASE("Cube", "[glb]") {
+  test_gltf("dependencies\\glTF-Sample-Models\\2.0\\Box\\glTF-Binary\\Box.glb");
 }
